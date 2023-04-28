819e443f0321f631c24c5447836bd3098dc44328
dev2007 <softengineerwu@gmail.com>
2023-04-28 17:25:19 +0800

    恢复 micronaut redis

--- src/main/java/fun/mortnon/framework/redis/RedisClientFactory.java
+++ src/main/java/fun/mortnon/framework/redis/RedisClientFactory.java
@@ -4,6 +4,7 @@ import io.lettuce.core.RedisClient;
 import io.lettuce.core.RedisURI;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.api.sync.RedisCommands;
+import io.micronaut.configuration.lettuce.DefaultRedisConfiguration;
 import io.micronaut.context.annotation.Bean;
 import io.micronaut.context.annotation.Factory;
 import io.micronaut.context.annotation.Requires;
@@ -15,22 +16,10 @@ import java.util.Optional;
  * @author dev2007
  * @date 2023/4/28
  */
-@Requires(beans = RedisConfiguration.class)
+@Requires(beans = DefaultRedisConfiguration.class)
 @Singleton
 @Factory
 public class RedisClientFactory {
-
-    @Bean(preDestroy = "shutdown")
-    public RedisClient redisClient(RedisConfiguration config) {
-        RedisURI uri = config.getUri();
-        return Optional.ofNullable(uri).map(RedisClient::create)
-                .orElseGet(() -> RedisClient.create());
-    }
-
-    @Bean(preDestroy = "close")
-    public StatefulRedisConnection<String, String> redisConnection(RedisClient redisClient) {
-        return redisClient.connect();
-    }
 
     @Bean
     public RedisCommands<String, String> redisCommands(StatefulRedisConnection<String, String> connection) {
