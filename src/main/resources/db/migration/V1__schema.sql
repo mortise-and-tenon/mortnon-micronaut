93e106b195896c46bbe684ee074a70523f57dbc2
awu <softengineerwu@gmail.com>
2023-12-05 00:23:33 +0800

    update micronaut;add sys_menu

--- src/main/resources/db/migration/V1__schema.sql
+++ src/main/resources/db/migration/V1__schema.sql
@@ -1,17 +1,17 @@
 BEGIN;
 -- 用户表
 CREATE TABLE IF NOT EXISTS `sys_user` (
-	`id` BIGINT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-	`user_name` VARCHAR(255) NOT NULL UNIQUE,
-	`nick_name` VARCHAR(255) NOT NULL,
-	`password` VARCHAR(255) NOT NULL,
-	`salt` VARCHAR(255) NOT NULL,
-	`email` VARCHAR(128) DEFAULT NULL,
-	`phone` VARCHAR(32) DEFAULT NULL,
-	`avatar` VARCHAR(1024) DEFAULT NULL,
-	`sex` TINYINT NOT NULL DEFAULT 1,
-	`gmt_create` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`gmt_modify` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+	`id` BIGINT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY  COMMENT '用户 id',
+	`user_name` VARCHAR(255) NOT NULL UNIQUE                COMMENT '用户名/登录名',
+	`nick_name` VARCHAR(255) NOT NULL                       COMMENT '用户昵称/姓名',
+	`password` VARCHAR(255) NOT NULL                        COMMENT '用户密码',
+	`salt` VARCHAR(255) NOT NULL                            COMMENT '密码加盐值',
+	`email` VARCHAR(128) DEFAULT NULL                       COMMENT '用户邮箱',
+	`phone` VARCHAR(32) DEFAULT NULL                        COMMENT '用户手机号',
+	`avatar` VARCHAR(1024) DEFAULT NULL                     COMMENT '用户头像',
+	`sex` TINYINT NOT NULL DEFAULT 1                        COMMENT '用户性别，0女，1男',
+	`gmt_create` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP                                COMMENT '创建时间',
+	`gmt_modify` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP    COMMENT '修改时间'
 );
 
 INSERT INTO `sys_user`(user_name,nick_name,password,salt,email,phone)
@@ -21,12 +21,12 @@ FROM DUAL WHERE NOT EXISTS(SELECT id FROM `sys_user`);
 
 -- 角色表
 CREATE TABLE IF NOT EXISTS `sys_role`(
-	`id` BIGINT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-	`name` VARCHAR(1024) NOT NULL,
-	`identifier` VARCHAR(64) NOT NULL,
-	`description` VARCHAR(1024) NULL,
-	`gmt_create` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`gmt_modify` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+	`id` BIGINT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY  COMMENT '角色 id',
+	`name` VARCHAR(1024) NOT NULL                           COMMENT '角色名字',
+	`identifier` VARCHAR(64) NOT NULL                       COMMENT '角色标识符',
+	`description` VARCHAR(1024) NULL                        COMMENT '角色描述',
+	`gmt_create` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP                                COMMENT '创建时间',
+	`gmt_modify` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP    COMMENT '修改时间'
 );
 
 INSERT INTO `sys_role`(name,identifier,description)
@@ -35,12 +35,12 @@ FROM DUAL WHERE NOT EXISTS(SELECT id FROM `sys_role`);
 
 -- 组织表
 CREATE TABLE IF NOT EXISTS `sys_project`(
-	`id` BIGINT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-	`name` VARCHAR(1024) NOT NULL,
-	`description` VARCHAR(1024) NULL,
-	`parent_id` BIGINT NULL,
-	`gmt_create` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`gmt_modify` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+	`id` BIGINT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY  COMMENT '组织 id',
+	`name` VARCHAR(1024) NOT NULL                           COMMENT '组织名字',
+	`description` VARCHAR(1024) NULL                        COMMENT '组织描述',
+	`parent_id` BIGINT NULL                                 COMMENT '父组织 id',
+	`gmt_create` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP                                COMMENT '创建时间',
+	`gmt_modify` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP    COMMENT '修改时间'
 );
 
 INSERT INTO `sys_project`(name,description,parent_id)
@@ -50,10 +50,10 @@ VALUES ('Mornton总公司','',0),
 
 -- 用户、角色、组织关联表
 CREATE TABLE IF NOT EXISTS `sys_assignment`(
-	`id` BIGINT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-	`user_id` BIGINT NOT NULL,
-	`project_id` BIGINT NOT NULL,
-	`role_id` BIGINT NOT NULL,
+	`id` BIGINT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY  COMMENT '关联 id',
+	`user_id` BIGINT NOT NULL                               COMMENT '用户 id',
+	`project_id` BIGINT NOT NULL                            COMMENT '组织 id',
+	`role_id` BIGINT NOT NULL                               COMMENT '角色 id',
 	FOREIGN KEY (user_id) REFERENCES sys_user(id),
 	FOREIGN KEY (role_id) REFERENCES sys_role(id),
 	FOREIGN KEY (project_id) REFERENCES sys_project(id)
@@ -66,14 +66,14 @@ FROM DUAL WHERE NOT EXISTS(SELECT id FROM `sys_assignment`);
 -- 权限表
 DROP TABLE IF EXISTS `sys_permission`;
 CREATE TABLE `sys_permission`(
-	`id` BIGINT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-	`name` VARCHAR(64) NOT NULL,
-	`identifier` VARCHAR(64) NOT NULL,
-	`description` VARCHAR(1024) NULL,
-	`api` VARCHAR(1024) NOT NULL,
-	`method` ENUM('GET','PUT','POST','DELETE','PATCH') NOT NULL DEFAULT 'GET',
-	`gmt_create` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`gmt_modify` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+	`id` BIGINT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY  COMMENT '权限 id',
+	`name` VARCHAR(64) NOT NULL                             COMMENT '权限名字',
+	`identifier` VARCHAR(64) NOT NULL                       COMMENT '权限标识符',
+	`description` VARCHAR(1024) NULL                        COMMENT '权限描述',
+	`api` VARCHAR(1024) NOT NULL                            COMMENT '权限控制的 api',
+	`method` ENUM('GET','PUT','POST','DELETE','PATCH') NOT NULL DEFAULT 'GET'   COMMENT '权限控制的 api 的方法',
+	`gmt_create` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP                                COMMENT '创建时间',
+	`gmt_modify` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP    COMMENT '修改时间'
 );
 
 INSERT INTO `sys_permission`(name,identifier,description,api,method)
@@ -96,13 +96,16 @@ VALUES ('查询用户','USER_QUERY','查看用户数据','/users/**','GET'),
     ('创建组织','PROJECT_CREATE','创建组织数据','/projects','POST'),
     ('修改组织','PROJECT_MODIFY','修改组织数据','/projects','PUT'),
     ('删除组织','PROJECT_DELETE','删除组织数据','/projects/**','DELETE'),
+    ('查询菜单','MENU_QUERY','查询菜单数据','/menus','GET'),
+    ('创建菜单','MENU_CREATE','创建菜单数据','/menus','POST'),
+    ('删除菜单','MENU_DELETE','删除菜单数据','/menus/**','DELETE'),
     ('查看日志','LOG_QUERY','查看日志数据','/logs','GET');
 
 -- 角色权限关联表
 CREATE TABLE IF NOT EXISTS `sys_role_permission`(
-	`id` BIGINT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-	`role_id` BIGINT NOT NULL,
-	`permission_id` BIGINT NOT NULL,
+	`id` BIGINT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY  COMMENT '关联 id',
+	`role_id` BIGINT NOT NULL                               COMMENT '角色 id',
+	`permission_id` BIGINT NOT NULL                         COMMENT '权限 id',
 	FOREIGN KEY (role_id) REFERENCES sys_role(id),
 	FOREIGN KEY (permission_id) REFERENCES sys_permission(id)
 );
@@ -113,16 +116,38 @@ FROM sys_permission;
 
 -- 操作日志表
 CREATE TABLE IF NOT EXISTS `sys_log` (
-	`id` BIGINT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
-	`action` VARCHAR(255) NOT NULL,
-	`user_name` VARCHAR(255) NOT NULL,
-	`project_name` VARCHAR(255) NULL,
-	`project_id` BIGINT NULL,
-	`ip` VARCHAR(255) NOT NULL,
-	`result` ENUM('SUCCESS',"FAILURE") DEFAULT 'SUCCESS',
-	`level` ENUM('INFO','WARN','DANGER') DEFAULT 'INFO',
-	`time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`gmt_create` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`gmt_modify` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+	`id` BIGINT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY  COMMENT '操作日志 id',
+	`action` VARCHAR(255) NOT NULL                          COMMENT '操作行为',
+	`user_name` VARCHAR(255) NOT NULL                       COMMENT '用户名',
+	`project_name` VARCHAR(255) NULL                        COMMENT '组织名',
+	`project_id` BIGINT NULL                                COMMENT '组织 id',
+	`ip` VARCHAR(255) NOT NULL                              COMMENT '操作 ip',
+	`result` ENUM('SUCCESS',"FAILURE") DEFAULT 'SUCCESS'    COMMENT '操作结果',
+	`level` ENUM('INFO','WARN','DANGER') DEFAULT 'INFO'     COMMENT '日志级别',
+	`time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP      COMMENT '操作时间',
+	`gmt_create` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP                                COMMENT '创建时间',
+	`gmt_modify` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP    COMMENT '修改时间'
 );
+
+-- 菜单表
+CREATE TABLE IF NOT EXISTS `sys_menu`(
+    `id` BIGINT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY  COMMENT '菜单 id',
+    `name` VARCHAR(64) NOT NULL                             COMMENT '菜单名字',
+    `parent_id` BIGINT DEFAULT 0 NOT NULL                   COMMENT '父菜单 id',
+    `order` INT(4) DEFAULT 0                                COMMENT '菜单排序',
+    `url` VARCHAR(200) NOT NULL                             COMMENT '菜单 url',
+    `icon` VARCHAR(100) NOT NULL DEFAULT ''                 COMMENT '菜单 图标',
+    `permission` VARCHAR(20) DEFAULT NULL                   COMMENT '权限组',
+    `gmt_create` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP                                COMMENT '创建时间',
+    `gmt_modify` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP    COMMENT '修改时间'
+);
+
+INSERT INTO `sys_menu` (`name`, `parent_id`, `url`, `permission`)
+VALUES
+	('用户管理', 0, '/user', '["USER_QUERY"]'),
+	('角色管理', 0, '/role', '["ROLE_QUERY"]'),
+	('组织管理', 0, '/project', '["PROJECT_QUERY"]'),
+	('菜单管理', 0, '/url', '["MENU_QUERY"]'),
+	('日志管理', 0, '/log', '["LOG_QUERY"]');
+
 COMMIT;
