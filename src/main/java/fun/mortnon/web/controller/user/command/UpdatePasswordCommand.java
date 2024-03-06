7f50dd5924f11975289577ea06c89ce7e63f045f
dev2007 <softengineerwu@gmail.com>
2024-03-06 10:41:26 +0800

    修改密码修改

--- src/main/java/fun/mortnon/web/controller/user/command/UpdatePasswordCommand.java
+++ src/main/java/fun/mortnon/web/controller/user/command/UpdatePasswordCommand.java
@@ -3,6 +3,7 @@ package fun.mortnon.web.controller.user.command;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.micronaut.core.annotation.Introspected;
 import io.micronaut.serde.annotation.Serdeable;
+import io.micronaut.serde.config.naming.SnakeCaseStrategy;
 import lombok.Data;
 
 import javax.validation.constraints.*;
@@ -12,7 +13,7 @@ import javax.validation.constraints.*;
  * @date 2023/2/17
  */
 @Introspected
-@Serdeable
+@Serdeable(naming = SnakeCaseStrategy.class)
 @Data
 public class UpdatePasswordCommand {
     /**
